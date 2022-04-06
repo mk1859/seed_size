2df6e42052a1872fda79795464b4062d56fb936e
mk1859 <mkrzyszton.wtm@gmail.com>
2022-04-06 15:54:23 +0200

    Update nreads_plot.R

--- R/nreads_plot.R
+++ R/nreads_plot.R
@@ -1,32 +1,30 @@
 # function to create a multipanel diagram, it takes order of treatments as input
 # require seed naming: treatment.library(01-99).batch(01-99)
 
-nreads_plot <- function(matrix, order) {
-  require (ggplot2)
+nreads_plot <- function(matrix, order, tableu) {
   require (cowplot)
   require (patchwork)
-  require (ggthemes)
-  
-  # metadata per seed
-  seed_attr <- data.frame(n_reads = colSums(matrix),                                    # number of reads
-                        n_gene = colSums(matrix > 0),                                   # number of genes
-                        timepoint = as.factor(gsub('.{0,6}$', '', colnames(matrix))))   # extract information about treatment from seed name
-                        
+  # data frame with statistics per seed
+  seed_attr <- data.frame(n_reads = colSums(matrix), # number of reads
+                        n_gene = colSums(matrix > 0), # number of genes
+                        # extract information about time point from seed name
+                        timepoint = as.factor(gsub('.{0,6}$', '', colnames(matrix))))
   
   # set order of treatments
-  seed_attr$timepoint <- factor(seed_attr$timepoint, levels = order)
+  seed_attr$timepoint <- factor(seed_attr$timepoint, 
+                              levels = order)
   
-  # plot for the  number of reads and genes
+  # plot for number of reads and number of genes
   g1 <- ggplot(seed_attr, aes(x=n_reads, y=n_gene, color = timepoint)) +
-   geom_point(size = 0.75) + 
-   scale_color_tableau() +
+   geom_point(size = 1) + 
+   scale_color_tableau(tableu) +
    theme_classic() +
    theme(legend.position = "none")
   
-  # boxplot for the number of genes
+  # boxplot for number of genes
   g2 <- ggplot(seed_attr, aes(x=timepoint, y=n_gene, color = timepoint)) +
    geom_boxplot() + 
-   scale_color_tableau() +
+   scale_color_tableau(tableu) +
    theme_classic() +
    theme(axis.line=element_blank(),
          axis.text.x=element_blank(),
@@ -36,10 +34,10 @@ nreads_plot <- function(matrix, order) {
          axis.title.y=element_blank()) +
    theme(legend.position = "none")
 
-  # boxplot for the number of reads
+  # boxplot for number of reads
   g3 <- ggplot(seed_attr, aes(x=timepoint, y=n_reads, color = timepoint)) +
    geom_boxplot() + 
-   scale_color_tableau() +
+   scale_color_tableau(tableu) +
    theme_classic() +
    theme(axis.line=element_blank(),
          axis.text.x=element_blank(),
@@ -53,9 +51,8 @@ nreads_plot <- function(matrix, order) {
   # spacer plot 
   s <- plot_spacer()
   
-  # create a multipanel plot
+  # create multipanel plot
   g <- plot_grid(g3, s, g1, g2, ncol = 2, align = "hv", 
                rel_heights = c(1,2), rel_widths = c(2,0.64))
-               
   return (g)
 }
