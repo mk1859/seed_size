cfd0d04f30b79827dcb6b7446043641fc23e4d2d
mk1859 <mkrzyszton.wtm@gmail.com>
2022-04-06 17:29:35 +0200

    Create background_plot.R

---
+++ R/background_plot.R
@@ -0,0 +1,20 @@
+#function to create boxplot, takes also order of time points as input
+background_plot <- function(matrix, order, background) {
+ # data frame with statistics per seed
+  seed_attr <- data.frame(n_reads = colSums(matrix), # number of reads
+                          background = background, # fraction of background
+                        # extract information about time point from seed name
+                        timepoint = as.factor(gsub('.{0,6}$', '', colnames(matrix))))
+  
+  # set order of treatments
+  seed_attr$timepoint <- factor(seed_attr$timepoint, 
+                              levels = order)
+  
+  # boxplot for  fraction of background
+  g <- ggplot(seed_attr, aes(x=timepoint, y=background, color = timepoint)) +
+   geom_boxplot() + 
+   scale_color_tableau("Green-Orange-Teal") +
+   theme_classic() 
+  
+  return (g)
+}
