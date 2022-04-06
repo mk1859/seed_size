f8a0069b35f940534714210ef678f2eb1ac01b2c
mk1859 <mkrzyszton.wtm@gmail.com>
2022-04-06 18:02:45 +0200

    Create pca_discrete.R

---
+++ R/pca_discrete.R
@@ -0,0 +1,29 @@
+# PCA plot with discrete values coloring
+
+pca_discrete <- function (seurat_obj, type = "timepoint", tableu ="Green-Orange-Teal", excluded = NULL) {
+  require (Seurat)
+  require (ggplot2)
+  require (ggthemes)
+  
+  # export data from the Seurat object
+  plot <- cbind (as.data.frame (Embeddings(object = seurat_obj, reduction = "pca"))  [,1:2], seurat_obj@meta.data)
+  
+  # exclude treatment if necessary
+  if (!is.null(excluded)){
+    plot <-  plot [-grep( excluded, rownames(plot)),]
+  }
+  
+  g <- ggplot(plot, aes(x=PC_1, y= PC_2, color = .data[[type]])) +
+            geom_point (size = 2) + 
+            scale_color_tableau(tableu) +
+            theme_classic() +
+            theme(axis.line=element_blank(),
+                  axis.text.x=element_blank(),
+                  axis.text.y=element_blank(),
+                  axis.ticks=element_blank(),
+                  axis.title.x=element_blank(),
+                  axis.title.y=element_blank()) +
+            theme(legend.title = element_blank())
+  
+  return (g)
+}
