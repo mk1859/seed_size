438c36097a17d5dc7f8640787065bed152c499bb
mk1859 <mkrzyszton.wtm@gmail.com>
2022-04-06 23:04:12 +0200

    Update sig_vs_sig.R

--- R/sig_vs_sig.R
+++ R/sig_vs_sig.R
@@ -11,16 +11,10 @@ sig_vs_sig <- function (seurat_obj, signature1, signature2, vis_obj = NULL, orde
   plot$timepoint <- factor(plot$timepoint, 
                            levels = order)
   
-  # exclude some time points if necessary
-  if (!is.null(excluded )){
-    plot <-  plot [-grep( excluded, rownames(plot)),]
-  }
-  
   g <- ggplot(plot, aes(x=.data[[signature1]], y= .data[[signature2]], color = timepoint)) +
     geom_point(size = 1.5) +
     theme_classic() +
     scale_color_tableau("Green-Orange-Teal")
   
   return (g)
-  
 }
